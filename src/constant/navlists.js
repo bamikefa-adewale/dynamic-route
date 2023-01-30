c3e34312e9228c2c97076abcc7b651f9ededf7fc
olatubora-felix <olatubora1996@gmail.com>
2023-01-30 11:55:53 +0100

    react router class and dynamic route

---
+++ src/constant/navlists.js
@@ -0,0 +1,29 @@
+export const routes = [
+  {
+    name: 'Home',
+    link: '/',
+  },
+  {
+    name: 'About',
+    link: '/about-us',
+  },
+  {
+    name: 'Contact',
+    link: '/contact-us',
+  },
+  {
+    name: 'Skills',
+    link: '/skills',
+  },
+]
+
+export const logins = [
+  {
+    name: 'Sign In',
+    link: '/login',
+  },
+  {
+    name: 'Sign Up',
+    link: '/signup',
+  },
+]
